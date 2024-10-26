20cd406443485f6748c30949facc188d4533dc54
pixalquarks <kashyaprakshat@gmail.com>
2024-10-26 23:16:12 +0530

    Added crow

--- Assets/Scripts/Crow/Crow.cs
+++ Assets/Scripts/Crow/Crow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,23 @@ namespace CozyChaos2024Fall
 {
     public class Crow : MonoBehaviour
     {
-        [SerializeField] private float flySpeed = 8f;
-        [SerializeField] private Vector3 destination;
+        [SerializeField] private float flySpeed = 15f;
+        [HideInInspector] public Crop target;
+
+        [HideInInspector] public CrowManager crowManager;
+
+
+
+        private Vector3 destination;
+        private bool isReturning;
+
+        private void Start()
+        {
+            destination = target.transform.position;
+            target.OnCropDestroyed += WhenCropDestroyed;
+        }
+
+        private float lastPeckedTime;
 
         private void Update()
         {
@@ -17,7 +33,28 @@ namespace CozyChaos2024Fall
                 transform.position = Vector3.MoveTowards(transform.position, destination, flySpeed * Time.deltaTime);
             }
 
+            if ((destination - transform.position).magnitude <= 0.1f)
+            {
+                if (isReturning)
+                {
+                    Destroy(gameObject);
+                    return;
+                }
+                if (Time.time - lastPeckedTime < crowManager.peckingSpeed) return;
+                lastPeckedTime = Time.time;
+                target.TakeDamage(1);
+            }
         }
 
+        private void OnDisable()
+        {
+            target.OnCropDestroyed -= WhenCropDestroyed;
+        }
+
+        private void WhenCropDestroyed(object sender, EventArgs e)
+        {
+            isReturning = true;
+            destination = crowManager.GetRandomEdgeLocation();
+        }
     }
 }
--- Assets/Scripts/Crow/CrowManager.cs
+++ Assets/Scripts/Crow/CrowManager.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using CozyChaos2024Fall;
 using UnityEngine;
 
 public class CrowManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float spawnDelay = 4f;
+    public float peckingSpeed = 3f;
+    [SerializeField] private BoxCollider2D spawnBoundary;
+    [SerializeField] private GameObject crowPrefab;
+
+
+    private List<Crow> _crows = new();
+
+    private CropManager _cropManager;
+
+    private void Start()
     {
-        
+        _cropManager = FindObjectOfType<CropManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private float lastSpawnedTime = 0f;
+
+    private void Update()
     {
-        
+        if (Time.time - lastSpawnedTime >= spawnDelay)
+        {
+            lastSpawnedTime = Time.time;
+            var crop = _cropManager.GetDamageableCrop();
+            if (crop != null)
+            {
+                SpawnCrow(crop);
+            }
+        }
+    }
+
+    private void SpawnCrow(Crop target)
+    {
+        var spawnLocation = GetRandomEdgeLocation();
+
+        var crow = Instantiate(crowPrefab, spawnLocation, Quaternion.identity).GetComponent<Crow>();
+        crow.target = target;
+        crow.crowManager = this;
+        _crows.Add(crow);
+    }
+
+
+    // Returns a random spawn location that is always on the edge of the collider
+    public Vector2 GetRandomEdgeLocation()
+    {
+        var bounds = spawnBoundary.bounds;
+
+        // Choose a random edge (top, bottom, left, or right)
+        int edge = Random.Range(0, 4);
+        var location = edge switch
+        {
+            // Top edge
+            0 => new Vector2(Random.Range(bounds.min.x, bounds.max.x), bounds.max.y),
+            // Bottom edge
+            1 => new Vector2(Random.Range(bounds.min.x, bounds.max.x), bounds.min.y),
+            // Left edge
+            2 => new Vector2(bounds.min.x, Random.Range(bounds.min.y, bounds.max.y)),
+            // Right edge
+            3 => new Vector2(bounds.max.x, Random.Range(bounds.min.y, bounds.max.y)),
+            _ => throw new System.ArgumentOutOfRangeException(),
+        };
+        return location;
     }
 }
